94b65d48b8cc7e55a82760665353e232b7ed9ac7
Sean Heintz <sean.heintz@gmail.com>
2023-05-17 22:01:03 -0600

    format

--- src/ast/statement.ts
+++ src/ast/statement.ts
@@ -1,16 +1,24 @@
 import { Expression } from './expression';
 export interface Visitor<R> {
-    visitExpressionStatement(expressionStatement: ExpressionStatement): R;
-    visitPrintStatement(printStatement: PrintStatement): R;
+  visitExpressionStatement(expressionStatement: ExpressionStatement): R;
+  visitPrintStatement(printStatement: PrintStatement): R;
 }
 export abstract class Statement {
-    abstract accept<R>(visitor: Visitor<R>): R;
+  abstract accept<R>(visitor: Visitor<R>): R;
 }
 export class ExpressionStatement extends Statement {
-    constructor(public Expression: Expression) { super(); }
-    accept<R>(visitor: Visitor<R>): R { return visitor.visitExpressionStatement(this); }
+  constructor(public Expression: Expression) {
+    super();
+  }
+  accept<R>(visitor: Visitor<R>): R {
+    return visitor.visitExpressionStatement(this);
+  }
 }
 export class PrintStatement extends Statement {
-    constructor(public expression: Expression) { super(); }
-    accept<R>(visitor: Visitor<R>): R { return visitor.visitPrintStatement(this); }
+  constructor(public expression: Expression) {
+    super();
+  }
+  accept<R>(visitor: Visitor<R>): R {
+    return visitor.visitPrintStatement(this);
+  }
 }
